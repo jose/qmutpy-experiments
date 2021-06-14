2c285df640cbdf44f2b225405801d38ae2cc5b50
jose <jcmcampos@fc.ul.pt>
2021-06-14 18:17:39 +0100

    Added script to run QMutPy on a single quantum algorithm.

---
+++ experiments/run_qmutpy.sh
@@ -0,0 +1,118 @@
+#!/usr/bin/env bash
+#
+# ------------------------------------------------------------------------------
+# This script runs QMutPy on a single quantum algorithm.
+#
+# Usage:
+# run_qmutpy.sh
+#   --quantum_framework_name <name, e.g., qiskit-aqua>
+#   --quantum_framework_root_path <path, e.g., $(pwd)/../tools/qiskit-aqua/>
+#   --pyenv_name <name, e.g., 3.7.0-qmutpy-and-qiskit-aqua>
+#   --algorithm_name <name, e.g., shor>
+#   --algorithm_file_path <path, e.g., qiskit/aqua/algorithms/factorizers/shor.py>
+#   --algorithm_test_suite_file_path <path, e.g., test/aqua/test_shor.py>
+#   --mutation_operator <name, e.g., QGD>
+#   --report_output_file <path, e.g., qiskit-aqua-shor-QGD.yaml>
+#   [help]
+#
+# Requirements:
+#   Execution of tools/get_tools.sh script.
+# ------------------------------------------------------------------------------
+
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+source "$SCRIPT_DIR/utils.sh" || exit 1
+
+# ------------------------------------------------------------------------- Envs
+
+# Init pyenv and pyenv-virtualenv
+_init_pyenv || die
+
+# ------------------------------------------------------------------------- Args
+
+USAGE="Usage: ${BASH_SOURCE[0]} --quantum_framework_name <name, e.g., qiskit-aqua> --quantum_framework_root_path <path, e.g., \$(pwd)/../tools/qiskit-aqua/> --pyenv_name <name, e.g., 3.7.0-qmutpy-and-qiskit-aqua> --algorithm_name <name, e.g., shor> --algorithm_path <path, e.g., qiskit/aqua/algorithms/factorizers/shor.py> --algorithm_test_suite_file_path <path, e.g., test/aqua/test_shor.py> --mutation_operator <name, e.g., QGD> --report_output_file <path, e.g., qiskit-aqua-shor-QGD.yaml> [help]"
+if [ "$#" -ne "16" ]; then
+  die "$USAGE"
+fi
+
+QUANTUM_FRAMEWORK_NAME=""
+QUANTUM_FRAMEWORK_ROOT_PATH=""
+PYENV_NAME=""
+ALGORITHM_NAME=""
+ALGORITHM_PATH=""
+ALGORITHM_TEST_SUITE_FILE_PATH=""
+MUTATION_OPERATOR=""
+REPORT_OUTPUT_FILE=""
+
+while [[ "$1" = --* ]]; do
+  OPTION=$1; shift
+  case $OPTION in
+    (--quantum_framework_name)
+      QUANTUM_FRAMEWORK_NAME=$1;
+      shift;;
+    (--quantum_framework_root_path)
+      QUANTUM_FRAMEWORK_ROOT_PATH=$1;
+      shift;;
+    (--pyenv_name)
+      PYENV_NAME=$1;
+      shift;;
+    (--algorithm_name)
+      ALGORITHM_NAME=$1;
+      shift;;
+    (--algorithm_path)
+      ALGORITHM_PATH=$1;
+      shift;;
+    (--algorithm_test_suite_file_path)
+      ALGORITHM_TEST_SUITE_FILE_PATH=$1;
+      shift;;
+    (--mutation_operator)
+      MUTATION_OPERATOR=$1;
+      shift;;
+    (--report_output_file)
+      REPORT_OUTPUT_FILE=$1;
+      shift;;
+    (--help)
+      echo "$USAGE"
+      exit 0
+    (*)
+      die "$USAGE";;
+  esac
+done
+
+# Check whether all mandatory arguments have been used
+[ "$QUANTUM_FRAMEWORK_NAME" != "" ]         || die "[ERROR] Missing --quantum_framework_name argument!"
+[ "$QUANTUM_FRAMEWORK_ROOT_PATH" != "" ]    || die "[ERROR] Missing --quantum_framework_root_path argument!"
+[ "$PYENV_NAME" != "" ]                     || die "[ERROR] Missing --pyenv_name argument!"
+[ "$ALGORITHM_NAME" != "" ]                 || die "[ERROR] Missing --algorithm_name argument!"
+[ "$ALGORITHM_PATH" != "" ]                 || die "[ERROR] Missing --algorithm_path argument!"
+[ "$ALGORITHM_TEST_SUITE_FILE_PATH" != "" ] || die "[ERROR] Missing --algorithm_test_suite_file_path argument!"
+[ "$MUTATION_OPERATOR" != "" ]              || die "[ERROR] Missing --mutation_operator argument!"
+[ "$REPORT_OUTPUT_FILE" != "" ]             || die "[ERROR] Missing --report_output_file argument!"
+
+# Check whether QUANTUM_FRAMEWORK_ROOT_PATH exits
+[ -d "$QUANTUM_FRAMEWORK_ROOT_PATH" ] || die "[ERROR] $QUANTUM_FRAMEWORK_ROOT_PATH does not exist!"
+# Check whether ALGORITHM_PATH exits
+[ -s "$QUANTUM_FRAMEWORK_ROOT_PATH/$ALGORITHM_PATH" ] || die "[ERROR] $QUANTUM_FRAMEWORK_ROOT_PATH/$ALGORITHM_PATH does not exist or it is empty!"
+# Check whether ALGORITHM_TEST_SUITE_FILE_PATH exits
+[ -s "$QUANTUM_FRAMEWORK_ROOT_PATH/$ALGORITHM_TEST_SUITE_FILE_PATH" ] || die "[ERROR] $QUANTUM_FRAMEWORK_ROOT_PATH/$ALGORITHM_TEST_SUITE_FILE_PATH does not exist or it is empty!"
+
+# ------------------------------------------------------------------------- Main
+
+echo "PID: $$"
+echo "Job started at $(date)"
+hostname
+
+pushd . > /dev/null 2>&1
+cd "$QUANTUM_FRAMEWORK_ROOT_PATH"
+  _load_pyenv "$PYENV_NAME" || die
+
+  mut.py --target "$ALGORITHM_PATH" --unit-test "$ALGORITHM_TEST_SUITE_FILE_PATH" -m --operator "$MUTATION_OPERATOR" --report "$REPORT_OUTPUT_FILE" || die "[ERROR] Failed to run QMutPy::$MUTATION_OPERATOR on $ALGORITHM_NAME!"
+  [ -s "$REPORT_OUTPUT_FILE" ] || die "[ERROR] $REPORT_OUTPUT_FILE does not exist or it is empty!"
+
+  _unload_pyenv || die
+popd > /dev/null 2>&1
+
+echo "Job finished at $(date)"
+echo "DONE!"
+exit 0
+
+# EOF
--- experiments/utils.sh
+++ experiments/utils.sh
@@ -15,4 +15,55 @@ die() {
   exit 1
 }
 
+#
+# Init [Simple Python Version Management: pyenv](https://github.com/pyenv/pyenv)
+# and [pyenv-virtualenv](https://github.com/pyenv/pyenv-virtualenv).
+#
+_init_pyenv() {
+  export PYENV_ROOT="$PWD/../tools/pyenv"
+  [ -d "$PYENV_ROOT" ] || die "[ERROR] $PYENV_ROOT does not exist!"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+
+  # Check whether `pyenv` is available
+  pyenv --version > /dev/null 2>&1 || die "[ERROR] Could not find 'pyenv'!"
+
+  # Init it
+  eval "$(pyenv init --path)" || die "[ERROR] Failed to init pyenv!"
+  eval "$(pyenv virtualenv-init -)" || die "[ERROR] Failed to init pyenv-virtualenv!"
+
+  return 0
+}
+
+#
+# Load a specific Python virtual environment.
+#
+_load_pyenv() {
+  local USAGE="Usage: ${FUNCNAME[0]} <env name>"
+  if [ "$#" != 1 ] ; then
+    echo "$USAGE" >&2
+    return 1
+  fi
+
+  local env_name="$1"
+  pyenv local "$env_name" || die "[ERROR] Failed to load the Python virtual environment '$env_name'!"
+  python --version >&2
+
+  return 0
+}
+
+#
+# Unload the loaded Python virtual environment.
+#
+_unload_pyenv() {
+  local USAGE="Usage: ${FUNCNAME[0]}"
+  if [ "$#" != 0 ] ; then
+    echo "$USAGE" >&2
+    return 1
+  fi
+
+  rm ".python-version" || die "[ERROR] Failed to unload the loaded Python virtual environment!"
+
+  return 0
+}
+
 # EOF
