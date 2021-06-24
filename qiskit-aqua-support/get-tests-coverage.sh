e17f51c7083c681dd4f626ce14f9959b999659be
jose <jcmcampos@fc.ul.pt>
2021-06-24 11:51:39 +0100

    Added script to collect Qiskit test suite's code coverage.

---
+++ qiskit-aqua-support/get-tests-coverage.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+#
+# ------------------------------------------------------------------------------
+# This script measures code coverage (at line level) of each test suite in the
+# Qiskit-Aqua project.  For each test suite this script generates a JSON report
+# under tests-coverage/, e.g., tests-coverage/qiskit.aqua.algorithms.amplitude_amplifiers.grover.json.
+# Once all test suites have been analyzed, this scripts collects all data in a
+# single CSV file (tests-coverage.csv) which follows the following format:
+#
+#    file,line,covered,excluded
+#    qiskit/aqua/algorithms/amplitude_amplifiers/grover.py,13,1,0
+#
+# - where file represents the file for which coverage was collected
+# - line represented the line number of a statement
+# - covered is either 1 if a line was exercised by the test suite, 0 otherwise
+# - excluded is either 1 if a line was excluded from coverage report, 0 otherwise
+#
+# Usage:
+# get-tests-coverage.sh
+#
+# Requirements:
+#   Execution of tools/get-tools.sh script.
+# ------------------------------------------------------------------------------
+
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+source "$SCRIPT_DIR/../experiments/utils.sh" || exit 1
+
+# ------------------------------------------------------------------------- Args
+
+QUANTUM_FRAMEWORK_ROOT_PATH="$SCRIPT_DIR/../tools/qiskit-aqua"
+# Check whether QUANTUM_FRAMEWORK_ROOT_PATH exits
+[ -d "$QUANTUM_FRAMEWORK_ROOT_PATH" ] || die "[ERROR] $QUANTUM_FRAMEWORK_ROOT_PATH does not exist!"
+
+QUANTUM_SUBJECTS_FILE_PATH="$SCRIPT_DIR/subjects.csv"
+# Check whether QUANTUM_SUBJECTS_FILE_PATH exits
+[ -s "$QUANTUM_SUBJECTS_FILE_PATH" ] || die "[ERROR] $QUANTUM_SUBJECTS_FILE_PATH does not exist or it is empty!"
+
+COVERAGE_DIR="$SCRIPT_DIR/tests-coverage"
+# Check whether COVERAGE_DIR exits
+[ -d "$COVERAGE_DIR" ] || die "[ERROR] $COVERAGE_DIR does not exist!"
+
+JSON_TO_CSV_SCRIPT="$COVERAGE_DIR/json2csv.py"
+# Check whether JSON_TO_CSV_SCRIPT exits
+[ -s "$JSON_TO_CSV_SCRIPT" ] || die "[ERROR] $JSON_TO_CSV_SCRIPT does not exist!"
+
+TEST_COVERAGE_CSV="$SCRIPT_DIR/tests-coverage.csv"
+rm -f "$TEST_COVERAGE_CSV"
+
+# ------------------------------------------------------------------------- Main
+
+echo "PID: $$"
+echo "Job started at $(date)"
+hostname
+
+pushd . > /dev/null 2>&1
+cd "$QUANTUM_FRAMEWORK_ROOT_PATH"
+  _activate_virtual_environment || die
+
+  while read -r subject_row; do
+    # algorithm_name,algorithm_full_name,test_suite_full_name
+    # grover,qiskit.aqua.algorithms.amplitude_amplifiers.grover,test.aqua.test_grover
+
+    algorithm_full_name=$(echo "$subject_row" | cut -f2 -d',')
+    algorithm_test_suite_full_name=$(echo "$subject_row" | cut -f3 -d',')
+    json_cov_file="$COVERAGE_DIR/$algorithm_full_name.json"
+    csv_cov_file="$COVERAGE_DIR/$algorithm_full_name.csv"
+
+    # Remove any previously collected coverage
+    rm -f "$json_cov_file" "$csv_cov_file" ".coverage"
+
+    # Run test suite and collect coverage
+    coverage run --source="$algorithm_full_name" -m unittest "$algorithm_test_suite_full_name" || die "[ERROR] Failed to collect $algorithm_full_name coverage!"
+    [ -s ".coverage" ] || die "[ERROR] .coverage does not exist or it is empty!"
+
+    # Print to stdout the coverage collected
+    coverage report -m "$(echo $algorithm_full_name | tr '.' '/').py" || die "[ERROR] Failed to print to stdout the coverage collected for $algorithm_full_name!"
+
+    # Print to a JSON file the coverage collected
+    coverage json -o "$json_cov_file" --pretty-print --include="$(echo $algorithm_full_name | tr '.' '/').py" || die "[ERROR] Failed to print to JSON file the coverage collected for $algorithm_full_name!"
+    [ -s "$json_cov_file" ] || die "[ERROR] $json_cov_file does not exist or it is empty!"
+
+    # Convert the auto generated JSON to CSV
+    python "$JSON_TO_CSV_SCRIPT" "$json_cov_file" "$csv_cov_file" || die "[ERROR] Failed to convert $json_cov_file into $csv_cov_file!"
+
+    # Collect data in a single CSV
+    if [ ! -f "$TEST_COVERAGE_CSV" ]; then
+      cat "$csv_cov_file" > "$TEST_COVERAGE_CSV" || die "[ERROR] Failed to create and populate $TEST_COVERAGE_CSV!"
+    else
+      tail -n +2 "$csv_cov_file" >> "$TEST_COVERAGE_CSV" || die "[ERROR] Failed to populate $TEST_COVERAGE_CSV!"
+    fi
+  done < <(tail -n +2 "$QUANTUM_SUBJECTS_FILE_PATH")
+
+  _deactivate_virtual_environment || die
+popd > /dev/null 2>&1
+
+echo "Job finished at $(date)"
+echo "DONE!"
+exit 0
+
+# EOF
