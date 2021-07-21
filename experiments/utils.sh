4c557b23ab222e89ed8c0f0d65b0944a191dbf4e
jose <jcmcampos@fc.ul.pt>
2021-07-21 16:58:02 +0100

    Quote path.

--- experiments/utils.sh
+++ experiments/utils.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 UTILS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
-USER_HOME_DIR=$(cd ~ && pwd)
+USER_HOME_DIR="$(cd ~ && pwd)"
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
